0885e8d5bb11998caad899ffa5a6d45099761d51
Ratchasri Kidking <orc_hero@hotmail.com>
2023-08-04 16:32:56 +0700

    compleate controller

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/auth' , (req,res) => {
-    res.send('Hello Auth Endpoint')
+    res.send('Login , Passport')
 })
 
 
--- Routes/product.js
+++ Routes/product.js
@@ -1,45 +1,18 @@
-// const express = require('express')
-// const router = express.Router()
-
-// const { read , list , create , update , remove } = require('../Controllers/product')
-
-// router.get('/product' , list)
-// router.get('/product/:id' , read)
-// router.post('/product' , create)
-// router.put('/product/:id' , update)
-// router.delete('/product/:id' , remove)
-
-// module.exports = router
-
 
 const express = require('express')
 const router = express.Router()
-
+const { read , list , create , remove , update  } = require('../Controllers/product')
 
 // ดึงข้อมูลทั้งหมด
-router.get('/product' , (req,res) => {
-    res.send('Ps5 , LG Sony')
-})
-
-router.get('/product/:id' , (req,res) => {
-    res.send('PS5 Joy Stick By ID')
-})
-
+router.get('/product' , read)
+router.get('/product/:id' , list)
 
 // ส่งข้อมูลเข้ามาบันทึก
-router.post('/product' , (req,res) => {
-    res.send('Post Product Endpoint ')
-})
-
+router.post('/product' , create)
 
 // Update ข้อมูล
-router.put('/product/:id' , (req,res) => {
-    res.send('Put product endpoint')
-})
+router.put('/product/:id', update)
 
-router.delete('/product/:id' , (req,res) => {
-    res.json({ name: 'Bom' , id: 1 })
-} )
-
+router.delete('/product/:id' , remove)
 
 module.exports = router
