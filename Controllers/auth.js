54c8c87f7a4ddf6e3e9e10ad67fa3f01f67f5526
Ratchasri Kidking <orc_hero@hotmail.com>
2023-08-13 15:11:16 +0700

    Register new User to db

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,17 +1,38 @@
+const User = require('../Models/User')
+const bcrypt = require('bcryptjs')
 
 exports.register = async(req,res)=>{
     try {
-        res.send('hello register')
+        //Destructuring Username and Password
+        const { username , password } = req.body
+        // ต้องใช้ var เพราะ User มีการเปลี่ยนแปลงค่า ถ้าเป็น const มันจะเป็นค่าคงที่
+        var user = await User.findOne({username})
+       
+        // Check new user if there is exist.
+        if(user){
+            return res.send('This user is already exists!!').status(400)
+        }
+        //2 Encrypt password
+        const salt = await bcrypt.genSalt(10)
+        user = new User({
+            username,
+            password
+        })
+        user.password = await bcrypt.hash(password , salt)
+
+        //3 Save to db
+        await user.save()
+        res.send('Register Successful')
+    
     } catch (error) {
         console.log('err')
         res.status(500)
         res.send('the server is not responding')
-        
     }
 }
 exports.login = async(req,res)=>{
     try {
-        res.send('hello register')
+        res.send('hello login controller')
     } catch (error) {
         console.log('err')
         res.status(500)
--- Models/authentication.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const userSchema = mongoose.Schema({
-    username:String,
-    password:String
-}, {timestamps: true })
-
-
-module.exports = mongoose.model('users',userSchema)
