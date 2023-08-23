3c9e27b3951e90ecf5f09275130c71ba6e995302
Ratchasri Kidking <orc_hero@hotmail.com>
2023-08-23 16:35:36 +0700

    Create Login Process

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,6 +1,9 @@
 const User = require('../Models/User')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+
+// Start Register 
 exports.register = async(req,res)=>{
     try {
         //Destructuring Username and Password
@@ -21,6 +24,7 @@ exports.register = async(req,res)=>{
             password
         })
         user.password = await bcrypt.hash(password , salt)
+        console.log(user)
 
         //3 Save to db
         await user.save()
@@ -32,15 +36,33 @@ exports.register = async(req,res)=>{
         res.send('the server is not responding')
     }
 }
-exports.login = async(req,res)=>{
+// End Register
+
+/// ---------------- Start Login ----------/// 
+
+exports.login = async(req,res) => {
     try {
-        res.send('hello login controller')
-    } catch (error) {
-        console.log('err')
-        res.status(500)
-        res.send('the server is not responding')
+        // 1 Check User if there is user in system already
+        const { name , password } = req.body
+        var user = await User.findOneAndUpdate({ name }, { new: true })
+        console.log(user)
         
+
+        if(user) {
+            const isMatch = await bcrypt.compare(password,user.password)
+
+            if(!isMatch){
+                return res.status(400).send('password is not Invalid')
+            }
+        }
+        res.send('hello login')
+        
+    } catch (error) {
+        console.log(error)
+        res.send(500).send('the server is not responding ')
     }
+
+    
 }
 
 
@@ -49,31 +71,5 @@ exports.login = async(req,res)=>{
 
 
 
-// const Users = require('../Models/authentication')
 
 
-
-// exports.createUser = async( req , res ) => {
-//     try {
-//         const newuser = await Users(req.body).save()
-//         res.send(newuser)
-//     } catch (error) {
-//         console.log(500).send('the server is not responding')
-//     }
-// }
-// exports.userid = async(req,res) => {
-//     try {
-//         await res.send('Ratchasri Kidking')
-//     } catch (error) {
-//         res.send(500).sent('the server is not responding')
-//     }
-// }
-
-
-
-
-// exports.createUser = async( req,res) => {
-//     console.log(req.body)
-//     const newUser = await CreateNewUser(req.body).save()
-//     res.send(newUser)
-// }
